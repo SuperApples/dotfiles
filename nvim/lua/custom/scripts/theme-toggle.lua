f9b8c432f7d7ac376d13799d43459ccb5f3280fb
Brett <brettcanfly+git@gmail.com>
2024-05-25 13:10:40 +1000

    leap fixed in script

--- nvim/lua/custom/scripts/theme-toggle.lua
+++ nvim/lua/custom/scripts/theme-toggle.lua
@@ -10,6 +10,8 @@ local current_theme_index = 1
 function M.toggle_theme()
 	current_theme_index = current_theme_index % #themes + 1
 	vim.cmd("colorscheme " .. themes[current_theme_index])
+	vim.api.nvim_set_hl(0, "LeapBackdrop", { link = "comment" })
+	vim.api.nvim_set_hl(0, "Cursor", { reverse = true })
 end
 
 return M
