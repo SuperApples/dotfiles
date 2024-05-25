c2b3b5bfd694a58603533ff760af99dcb0e40bd0
Brett <brettcanfly+git@gmail.com>
2024-05-25 10:35:11 +1000

    leap search area highlighting

--- nvim/lua/custom/plugins/leap.lua
+++ nvim/lua/custom/plugins/leap.lua
@@ -1,18 +1,19 @@
-
 return {
-    {
-        "ggandor/leap.nvim",
-        config = function()
-            require('leap').create_default_mappings()
-        end
+	{
+		"ggandor/leap.nvim",
+		config = function()
+			require("leap").create_default_mappings()
+		end,
 
-        -- TUTORIAL:
-        -- s = forward
-        -- S = backward
-        --
-        -- Enter 2 chars
-        --
-        -- space or tab to go forward back
-    }
+		-- TUTORIAL:
+		-- s = forward
+		-- S = backward
+		--
+		-- Enter 2 chars
+		--
+		-- space or tab to go forward back
+	},
+
+	vim.api.nvim_set_hl(0, "LeapBackdrop", { link = "comment" }),
+	vim.api.nvim_set_hl(0, "Cursor", { reverse = true }),
 }
-
--- nvim/lua/lazy-plugins.lua
+++ nvim/lua/lazy-plugins.lua
@@ -12,10 +12,9 @@
 require("lazy").setup({
 	"tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
 
-	-- "gc" to comment visual regions/lines
 	{ "numToStr/Comment.nvim", opts = {} },
 
- 	require("kickstart/plugins/which-key"),
+	require("kickstart/plugins/which-key"),
 
 	require("kickstart/plugins/telescope"),
 
@@ -32,8 +31,7 @@ require("lazy").setup({
 	require("kickstart/plugins/treesitter"),
 
 	require("kickstart.plugins.debug"),
-	-- require 'kickstart.plugins.indent_line',
-	-- require 'kickstart.plugins.lint',
+	require("kickstart.plugins.lint"),
 	require("kickstart.plugins.autopairs"),
 	require("kickstart.plugins.neo-tree"),
 
